82b3fa01c8ed02341174b4db2fc3fd8613933829
Jared <jchacon00466@ufide.ac.cr>
2025-11-07 15:15:17 -0600

    feat: initial commit - gtm-api backend

---
+++ src/main/java/io/github/jaredmcc4/gtm/GtmApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jaredmcc4.gtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtmApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jaredmcc4/gtm/GtmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaredmcc4.gtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
